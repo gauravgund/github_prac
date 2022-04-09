15c588e68bff14d55821282f9fcfbd9ff2fa02a8
ggund <gauravgund2@gmail.com>
2022-04-09 12:22:44 +0530

    DF initialise

--- python_script1.py
+++ python_script1.py
@@ -1,8 +1,5 @@
 import pandas as pd
+l1 = [i for i in range(4)]
 
-l = ['a', 'b', 'c']
-l1 = [i for i in range(3)]
+df = pd.DataFrame({'col1':l1})
 
-df = pd.DataFrame({'col1': l, 'col2':l1})
-
-df.head(2)
